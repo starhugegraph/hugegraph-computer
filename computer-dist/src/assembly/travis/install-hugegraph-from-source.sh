e7d67c2e1bbe5fc55f2290fc8e5797faada5a942
imbajin <imbajin@users.noreply.github.com>
2021-10-25 21:34:48 +0800

    chore: update server url & adopt etcd

--- computer-dist/src/assembly/travis/install-hugegraph-from-source.sh
+++ computer-dist/src/assembly/travis/install-hugegraph-from-source.sh
@@ -8,11 +8,11 @@ if [[ $# -ne 1 ]]; then
 fi
 
 COMMIT_ID=$1
-HUGEGRAPH_GIT_URL="https://github.com/hugegraph/hugegraph.git"
+HUGEGRAPH_GIT_URL="https://github.com/starhugegraph/hugegraph.git"
 
 git clone --depth 100 ${HUGEGRAPH_GIT_URL}
 cd hugegraph
-git checkout ${COMMIT_ID}
+git checkout "${COMMIT_ID}"
 mvn package -DskipTests
 mv hugegraph-*.tar.gz ../
 cd ../
