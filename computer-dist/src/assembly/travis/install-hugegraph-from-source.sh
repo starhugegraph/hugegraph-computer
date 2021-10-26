81c12d6e6bcb1bb65f75064265d88ae176eb86df
coderzc <zc1217zc@126.com>
2021-10-26 12:58:40 +0800

    Update install-hugegraph-from-source.sh

--- computer-dist/src/assembly/travis/install-hugegraph-from-source.sh
+++ computer-dist/src/assembly/travis/install-hugegraph-from-source.sh
@@ -20,9 +20,8 @@ cd ../
 rm -rf hugegraph
 tar -zxf hugegraph-*.tar.gz
 
-sed -i "s/rpc.server_port=.*/rpc.server_port=8390/g" conf/rest-server.properties
-
 cd "$(find hugegraph-* | head -1)"
+sed -i "s/rpc.server_port=.*/rpc.server_port=8390/g" conf/rest-server.properties
 bin/init-store.sh || exit 1
 bin/start-hugegraph.sh || exit 1
 cd ../
