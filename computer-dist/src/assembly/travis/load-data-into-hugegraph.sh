b64dcf32314c9708fb1fde031ffcfccd0a3f0d0a
imbajin <imbajin@users.noreply.github.com>
2021-10-27 15:07:01 +0800

    Update load-data-into-hugegraph.sh

--- computer-dist/src/assembly/travis/load-data-into-hugegraph.sh
+++ computer-dist/src/assembly/travis/load-data-into-hugegraph.sh
@@ -2,10 +2,10 @@
 
 set -ev
 
-TRAVIS_DIR=`dirname $0`
+TRAVIS_DIR=$(dirname $0)
 DATASET_DIR=${TRAVIS_DIR}/../dataset
 
-HUGEGRAPH_LOADER_GIT_URL="https://github.com/hugegraph/hugegraph-loader.git"
+HUGEGRAPH_LOADER_GIT_URL="https://github.com/starhugegraph/hugegraph-loader.git"
 
 git clone --depth 10 ${HUGEGRAPH_LOADER_GIT_URL}
 
@@ -16,6 +16,6 @@ tar -zxf hugegraph-loader-*.tar.gz || exit 1
 cd ../
 
 wget http://files.grouplens.org/datasets/movielens/ml-latest-small.zip
-unzip -d ${DATASET_DIR} ml-latest-small.zip
+unzip -d "${DATASET_DIR}" ml-latest-small.zip
 
-hugegraph-loader/hugegraph-loader-*/bin/hugegraph-loader.sh -g hugegraph -f ${DATASET_DIR}/struct.json -s ${DATASET_DIR}/schema.groovy || exit 1
+hugegraph-loader/hugegraph-loader-*/bin/hugegraph-loader.sh -g hugegraph -f "${DATASET_DIR}"/struct.json -s "${DATASET_DIR}"/schema.groovy || exit 1
