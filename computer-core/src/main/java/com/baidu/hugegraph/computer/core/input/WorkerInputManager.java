5d9c5a93cda51f48479f9d84d28a639eb2745071
yandababa2 <20913165@qq.com>
2021-10-28 15:08:18 +0800

    yanheling test commit

--- computer-core/src/main/java/com/baidu/hugegraph/computer/core/input/WorkerInputManager.java
+++ computer-core/src/main/java/com/baidu/hugegraph/computer/core/input/WorkerInputManager.java
@@ -92,5 +92,6 @@ public class WorkerInputManager implements Manager {
             this.sendManager.sendEdge(vertex);
         }
         this.sendManager.finishSend(MessageType.EDGE);
+        //inverse edges here
     }
 }
