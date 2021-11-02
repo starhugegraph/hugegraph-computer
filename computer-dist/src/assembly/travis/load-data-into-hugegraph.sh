a0183bf6efe7606ec1f73ed31eb90ce1961ba04e
imbajin <imbajin@users.noreply.github.com>
2021-11-02 19:12:11 +0800

    Update load-data-into-hugegraph.sh

--- computer-dist/src/assembly/travis/load-data-into-hugegraph.sh
+++ computer-dist/src/assembly/travis/load-data-into-hugegraph.sh
@@ -11,7 +11,7 @@ git clone -b "${BRANCH}" --depth 10 ${HUGEGRAPH_LOADER_GIT_URL}
 
 cd hugegraph-loader
 mvn install:install-file -Dfile=assembly/static/lib/ojdbc8-12.2.0.1.jar -DgroupId=com.oracle -DartifactId=ojdbc8 -Dversion=12.2.0.1 -Dpackaging=jar || exit 1
-mvn package -DskipTests || exit 1
+mvn clean package -DskipTests || exit 1
 tar -zxf hugegraph-loader-*.tar.gz || exit 1
 cd ../
 
@@ -19,3 +19,5 @@ wget http://files.grouplens.org/datasets/movielens/ml-latest-small.zip
 unzip -d "${DATASET_DIR}" ml-latest-small.zip
 
 hugegraph-loader/hugegraph-loader-*/bin/hugegraph-loader.sh -g hugegraph -f "${DATASET_DIR}"/struct.json -s "${DATASET_DIR}"/schema.groovy || exit 1
+
+echo "Load finished, continue to next step"
