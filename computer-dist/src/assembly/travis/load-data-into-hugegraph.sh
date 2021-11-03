9d12469cdaed1e1bb3b88adb1c75b71d1a5658c4
imbajin <imbajin@users.noreply.github.com>
2021-11-03 10:27:47 +0800

    Update load-data-into-hugegraph.sh

--- computer-dist/src/assembly/travis/load-data-into-hugegraph.sh
+++ computer-dist/src/assembly/travis/load-data-into-hugegraph.sh
@@ -12,7 +12,8 @@ git clone -b "${BRANCH}" --depth 10 ${HUGEGRAPH_LOADER_GIT_URL}
 
 cd hugegraph-loader
 mvn install:install-file -Dfile=$LIB/ojdbc8-12.2.0.1.jar -DgroupId=com.oracle -DartifactId=ojdbc8 -Dversion=12.2.0.1 -Dpackaging=jar
-mvn install:install-file -Dfile=$LIB/hugegraph-client-1.9.7.jar -DgroupId=com.baidu.hugegraph -DartifactId=hugegraph-client -Dversion=1.9.7 -Dpackaging=jar -DpomFile=$LIB/loader-pom.xml
+mvn install:install-file -Dfile=$LIB/hugegraph-client-1.9.7.jar -DgroupId=com.baidu.hugegraph -DartifactId=hugegraph-client -Dversion=1.9.7 -Dpackaging=jar -DpomFile=$LIB/client-pom.xml
+mvn install:install-file -Dfile=$LIB/hugegraph-common-1.8.9.jar -DgroupId=com.baidu.hugegraph -DartifactId=hugegraph-common -Dversion=1.8.9 -Dpackaging=jar -DpomFile=$LIB/common-pom.xml
 mvn clean package -DskipTests || exit 1
 tar -zxf hugegraph-loader-*.tar.gz || exit 1
 cd ../
