279b773868b5a5f5eab3380866fe178bc08fc95e
yandababa2 <20913165@qq.com>
2021-11-11 17:36:22 +0800

    change output test

--- computer-test/src/main/java/com/baidu/hugegraph/computer/algorithm/centrality/closeness/ClosenessCentralityTest.java
+++ computer-test/src/main/java/com/baidu/hugegraph/computer/algorithm/centrality/closeness/ClosenessCentralityTest.java
@@ -32,7 +32,7 @@ import com.baidu.hugegraph.driver.HugeClient;
 import com.baidu.hugegraph.driver.SchemaManager;
 import com.baidu.hugegraph.structure.constant.T;
 import com.baidu.hugegraph.structure.graph.Vertex;
-import com.baidu.hugegraph.testutil.Assert;
+//import com.baidu.hugegraph.testutil.Assert;
 import com.google.common.collect.ImmutableMap;
 
 public class ClosenessCentralityTest extends AlgorithmTestBase {
