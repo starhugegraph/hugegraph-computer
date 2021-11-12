69a0b383d988bc3e88fbdf9052bcb5acf56b7eb3
yandababa2 <20913165@qq.com>
2021-11-12 11:50:40 +0800

    computer-test/src/main/java/com/baidu/hugegraph/computer/algorithm/centrality/betweenness/BetweennessCentralityTest.java

--- computer-test/src/main/java/com/baidu/hugegraph/computer/algorithm/centrality/betweenness/BetweennessCentralityTest.java
+++ computer-test/src/main/java/com/baidu/hugegraph/computer/algorithm/centrality/betweenness/BetweennessCentralityTest.java
@@ -21,8 +21,8 @@ package com.baidu.hugegraph.computer.algorithm.centrality.betweenness;
 
 import java.util.Map;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+//import org.junit.AfterClass;
+//import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.baidu.hugegraph.computer.algorithm.AlgorithmTestBase;
@@ -51,7 +51,7 @@ public class BetweennessCentralityTest extends AlgorithmTestBase {
                                      .put(7L, 0.0D)
                                      .build();
 
-    @BeforeClass
+    //@BeforeClass
     public static void setup() {
         clearAll();
 
@@ -115,7 +115,7 @@ public class BetweennessCentralityTest extends AlgorithmTestBase {
         v7.addEdge("link", v6);
     }
 
-    @AfterClass
+    //@AfterClass
     public static void clear() {
         clearAll();
     }
@@ -140,8 +140,8 @@ public class BetweennessCentralityTest extends AlgorithmTestBase {
             LOG.info("result is {} {}", result.id(), expect);
             LOG.info("property is {} {}", result.properties(), super.name()); 
             Assert.assertNotNull(expect);
-            //assertDoubleEquals(expect,
-            //  (double) result.property(super.name()));
+            assertDoubleEquals(expect,
+              (double) result.property(super.name()));
             return result;
         }
     }
