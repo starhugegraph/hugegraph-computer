bb7a6704859167e40eb6337b2336d90370793136
zxxxhonest <86346642+zxxxhonest@users.noreply.github.com>
2022-05-31 10:43:51 +0800

    Json error (#166)
    * fix json error
    
    * fix json error
    
    Co-authored-by: zhaoxiangxin <zhaoxiangxin@MacintoshdeMacBook-Pro.local>

--- computer-core/src/main/java/com/baidu/hugegraph/computer/core/util/JsonUtil.java
+++ computer-core/src/main/java/com/baidu/hugegraph/computer/core/util/JsonUtil.java
@@ -25,7 +25,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.guava;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
 
 public class JsonUtil {
 
